5dee45be7ba2c61ce858a135135d88e6a490ce10
Lennard Golsch <lennard.golsch@icloud.com>
2022-07-26 17:16:21 +0200

    started switching to modal

--- src/main/vue/contact-manager/src/components/Table.vue
+++ src/main/vue/contact-manager/src/components/Table.vue
@@ -5,52 +5,40 @@
         <th v-for="field in fields" :key="field.key" :class="field.class">
           {{ field.label }}
         </th>
+        <th class="col-1">
+          <!-- {{ t('digibib.contact.frontend.manager.label.actions') }} -->
+        </th>
       </tr>
     </thead>
     <tbody v-if="requests">
-      <template v-for="(item, index) in requests" :key="item">
-        <tr class="col-1" @click="toggleExpanded(index)">
-          <td v-for="field in fields" :key="field.key" :class="field.class">
-            {{ item[field.key] }}
-          </td>
-        </tr>
-        <tr v-if="expanded.includes(index)" class="bg-light-grey">
-          <td :colspan="fields.length">
-            <div class="row justify-content-md-center">
-              <div class="col-10">
-                <div class="row">
-                  <div class="col-12">
-                    <textarea rows="5" class="form-control" readonly v-model="item.message" />
-                  </div>
-                </div>
-                <div class="row mt-2 text-right">
-                  <div class="col">
-                    <button type="button" class="btn btn-danger mr-1">
-                      {{ $t('digibib.contact.frontend.manager.button.reject') }}
-                    </button>
-                    <button type="button" class="btn btn-success">
-                      {{ $t('digibib.contact.frontend.manager.button.accept') }}
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </template>
+      <tr v-for="(item, index) in requests" :key="item">
+        <td v-for="field in fields" :key="field.key" :class="field.class">
+          {{ item[field.key] }}
+        </td>
+        <td class="col-1">
+          <span class="icon" @click="viewTask(index)">
+            <i class="fa fa-eye"></i>
+          </span>
+          <span class="icon" @click="editTask(index)">
+            <i class="fa fa-edit"></i>
+          </span>
+          <span class="icon" @click="removeTask(index)">
+            <i class="fa fa-trash"></i>
+          </span>
+        </td>
+      </tr>
     </tbody>
   </table>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 import { useI18n } from 'vue-i18n';
 
 const store = useStore();
 const requests = computed(() => store.state.requests);
 const { t } = useI18n();
-const expanded = ref([]) as number[];
 const fields = [
   {
     key: 'created',
@@ -60,17 +48,17 @@ const fields = [
   {
     key: 'objectID',
     label: t('digibib.contact.frontend.manager.label.objectID'),
-    class: 'col-2',
+    class: 'col-1',
   },
   {
     key: 'name',
     label: t('digibib.contact.frontend.manager.label.name'),
-    class: 'col-3',
+    class: 'col-2',
   },
   {
     key: t('email'),
     label: t('digibib.contact.frontend.manager.label.email'),
-    class: 'col-2',
+    class: 'col-3',
   },
   {
     key: 'orcid',
@@ -83,12 +71,4 @@ const fields = [
     class: 'col-1',
   },
 ];
-function toggleExpanded(id: number) {
-  const index = this.expanded.indexOf(id);
-  if (index > -1) {
-    this.expanded.splice(index, 1);
-  } else {
-    this.expanded.push(id);
-  }
-}
 </script>
