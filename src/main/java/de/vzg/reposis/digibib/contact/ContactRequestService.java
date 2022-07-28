8f9ceee11af6a360c65a83d6d06fcca8aca36f1b
Lennard Golsch <lennard.golsch@icloud.com>
2022-07-28 11:58:01 +0200

    added license and fixes

--- src/main/java/de/vzg/reposis/digibib/contact/ContactRequestService.java
+++ src/main/java/de/vzg/reposis/digibib/contact/ContactRequestService.java
@@ -1,44 +1,38 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.vzg.reposis.digibib.contact;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
-import de.vzg.reposis.digibib.contact.exception.InvalidContactRequestException;
 import de.vzg.reposis.digibib.contact.exception.ContactRequestNotFoundException;
+import de.vzg.reposis.digibib.contact.exception.InvalidContactRequestException;
 import de.vzg.reposis.digibib.contact.model.ContactRequest;
 import de.vzg.reposis.digibib.contact.model.ContactRequestState;
 import de.vzg.reposis.digibib.contact.validation.ValidationHelper;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.mycore.common.MCRConstants;
 import org.mycore.common.MCRException;
-import org.mycore.common.MCRMailer;
-import org.mycore.common.MCRMailer.EMail;
 import org.mycore.common.MCRSessionMgr;
-import org.mycore.common.config.MCRConfiguration2;
-import org.mycore.common.events.MCRShutdownHandler;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
-import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
-import org.mycore.mods.MCRMODSWrapper;
-import org.mycore.orcid.user.MCRORCIDUser;
-import org.mycore.user2.MCRUser;
-import org.mycore.user2.MCRUserManager;
-import org.mycore.util.concurrent.MCRTransactionableRunnable;
 
 public class ContactRequestService {
-
-    private static final Logger LOGGER = LogManager.getLogger();
 
     private ContactRequestDAO contactRequestDAO;
 
