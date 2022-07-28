f8ef508b80f2667e038690c22e783f3109acb8d8
Lennard Golsch <lennard.golsch@icloud.com>
2022-07-28 12:30:53 +0200

    added dao interface

--- src/main/java/de/vzg/reposis/digibib/contact/ContactRequestDAO.java
+++ src/main/java/de/vzg/reposis/digibib/contact/ContactRequestDAO.java
@@ -20,49 +20,16 @@ package de.vzg.reposis.digibib.contact;
 
 import java.util.Collection;
 
-import javax.persistence.EntityManager;
-
 import de.vzg.reposis.digibib.contact.model.ContactRequest;
 import de.vzg.reposis.digibib.contact.model.ContactRequestState;
 
-import org.mycore.backend.jpa.MCREntityManagerProvider;
 import org.mycore.datamodel.metadata.MCRObjectID;
 
-public class ContactRequestDAO {
-
-    public Collection<ContactRequest> findAll() {
-        final Collection<ContactRequest> contactRequests = MCREntityManagerProvider.getCurrentEntityManager()
-                .createNamedQuery("ContactRequest.findAll", ContactRequest.class).getResultList();
-        return contactRequests;
-    }
-
-    public Collection<ContactRequest> findByObjectID(MCRObjectID objectID) {
-        final Collection<ContactRequest> contactRequests = MCREntityManagerProvider.getCurrentEntityManager()
-                .createNamedQuery("ContactRequest.findByObjectID", ContactRequest.class)
-                .setParameter("objectID", objectID).getResultList();
-        return contactRequests;
-    }
-
-    public Collection<ContactRequest> findByState(ContactRequestState state) {
-        final Collection<ContactRequest> contactRequests = MCREntityManagerProvider.getCurrentEntityManager()
-                .createNamedQuery("ContactRequest.findByState", ContactRequest.class)
-                .setParameter("state", state).getResultList();
-        return contactRequests;
-    }
-
-    public ContactRequest findByID(long id) {
-        final ContactRequest contactRequest = MCREntityManagerProvider.getCurrentEntityManager()
-            .find(ContactRequest.class, id);
-        return contactRequest;
-    }
-
-    public void save(ContactRequest contactRequest) {
-        MCREntityManagerProvider.getCurrentEntityManager().persist(contactRequest);
-    }
-
-    public void remove(ContactRequest contactRequest) {
-        final EntityManager entityManager = MCREntityManagerProvider.getCurrentEntityManager();
-        entityManager.remove(entityManager.contains(contactRequest) ? contactRequest
-                : entityManager.merge(contactRequest));
-    }
+public interface ContactRequestDAO {
+    Collection<ContactRequest> findAll();
+    Collection<ContactRequest> findByObjectID(MCRObjectID objectID);
+    Collection<ContactRequest> findByState(ContactRequestState state);
+    ContactRequest findByID(long id);
+    void save(ContactRequest contactRequest);
+    void remove(ContactRequest contactRequest);
 }
