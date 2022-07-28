edb19e04f35d07301b141a0366870e55046468f8
Lennard Golsch <lennard.golsch@icloud.com>
2022-07-28 11:58:01 +0200

    added license and fixes

--- src/main/java/de/vzg/reposis/digibib/contact/validation/ValidationHelper.java
+++ src/main/java/de/vzg/reposis/digibib/contact/validation/ValidationHelper.java
@@ -1,8 +1,23 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.vzg.reposis.digibib.contact.validation;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
@@ -14,7 +29,6 @@ public class ValidationHelper {
     public static boolean validateContactRequest(ContactRequest contactRequest) {
         final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         final Validator validator = factory.getValidator();
-        final Set<ConstraintViolation<ContactRequest>> constraintViolations = validator.validate(contactRequest);
-        return constraintViolations.size() == 0;
+        return validator.validate(contactRequest).size() == 0;
     }
 }
