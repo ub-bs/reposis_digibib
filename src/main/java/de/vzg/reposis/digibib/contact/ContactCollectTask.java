33103f3cffb34b30eccdcfc8f28db01e5056abdc
Lennard Golsch <lennard.golsch@icloud.com>
2022-08-04 18:19:19 +0200

    switched to cronjob ;)

--- src/main/java/de/vzg/reposis/digibib/contact/ContactCollectTask.java
+++ src/main/java/de/vzg/reposis/digibib/contact/ContactCollectTask.java
@@ -18,82 +18,38 @@
 
 package de.vzg.reposis.digibib.contact;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.Callable;
 
-import de.vzg.reposis.digibib.contact.ContactRequestService;
-import de.vzg.reposis.digibib.contact.exception.ContactRequestNotFoundException;
-import de.vzg.reposis.digibib.contact.model.ContactRequest;
 import de.vzg.reposis.digibib.contact.model.ContactRequestRecipient;
-import de.vzg.reposis.digibib.contact.model.ContactRequestState;
 import de.vzg.reposis.digibib.contact.model.RecipientSource;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import org.mycore.common.MCRSystemUserInformation;
-import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.mods.MCRMODSWrapper;
-import org.mycore.util.concurrent.MCRFixedUserCallable;
 
-public class ContactCollectTask implements Runnable {
+public class ContactCollectTask implements Callable<List<ContactRequestRecipient>> {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    private final MCRObjectID objectID;
 
-    private static final String FALLBACK_EMAIL = MCRConfiguration2
-            .getStringOrThrow(ContactConstants.CONF_PREFIX + "FallbackEmail");
-
-    private final ContactRequestService contactRequestService;
-
-    public ContactCollectTask() {
-        this.contactRequestService = ContactRequestService.getInstance();
+    public ContactCollectTask(MCRObjectID objectID) {
+        this.objectID = objectID;
     }
 
     @Override
-    public void run() {
-        LOGGER.info("Running contact collection cron...");
-        final Map<MCRObjectID, List<ContactRequestRecipient>> recipientsCache = new HashMap();
-        contactRequestService.listContactRequestsByState(ContactRequestState.RECEIVED).stream().forEach((r) -> {
-            LOGGER.info("Collecting recipients for {}", r.getId());
-            final List<ContactRequestRecipient> cachedRecipients = recipientsCache.get(r.getObjectID());
-            if (cachedRecipients != null) {
-                cachedRecipients.forEach((v) -> r.addRecipient(v));
-            } else {
-                try {
-                    new MCRFixedUserCallable<>(() -> { // use own transaction for each request to isolate errors
-                        collectRecipients(r);
-                        recipientsCache.put(r.getObjectID(), r.getRecipients());
-                        return null;
-                    }, MCRSystemUserInformation.getJanitorInstance()).call();
-                } catch (Exception e) {
-                    LOGGER.error(e);
-                }
-            }
-        });
+    public List<ContactRequestRecipient> call() {
+        return collectRecipients();
     }
 
-    private void collectRecipients(ContactRequest contactRequest) {
-        try {
-            if (contactRequest.getRecipients().isEmpty()) {
-                addFallbackRecipient(contactRequest);
-            }
-            contactRequest.setState(ContactRequestState.READY);
-        } catch (ContactRequestNotFoundException e) {
-            // nothing to do
-        } finally {
-            try {
-                contactRequestService.updateContactRequest(contactRequest);
-            } catch (ContactRequestNotFoundException e) {
-                // nothing to do
-            }
-        }
+    private List<ContactRequestRecipient> collectRecipients() {
+        final List<ContactRequestRecipient> recipients = new ArrayList();
+        // addOrcidRecipients
+        return recipients;
     }
 
-    private void addOrcidRecipients(ContactRequest contactRequest) {
-        final MCRObjectID objectID = contactRequest.getObjectID();
+    private void addOrcidRecipients() {
         final MCRObject object = MCRMetadataManager.retrieveMCRObject(objectID);
         /* getAuthors(object).stream().forEach((a) -> {
             final String orcid = getORCID(a);
@@ -101,12 +57,5 @@ public class ContactCollectTask implements Runnable {
                 recipients.addAll(getEmails(orcid));
             }
         }); */
-    }
-
-    private void addFallbackRecipient(ContactRequest contactRequest) {
-        ContactRequestRecipient fallback =
-                new ContactRequestRecipient("FDM Team", RecipientSource.FALLBACK, FALLBACK_EMAIL);
-        fallback.setContactRequest(contactRequest);
-        contactRequest.addRecipient(fallback);
     }
 }
