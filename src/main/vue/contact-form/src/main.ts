ce6fc8cbcd7b4163c23bddfc1bd407235020e87a
Lennard Golsch <lennard.golsch@icloud.com>
2022-08-17 11:46:09 +0200

    switch to setup approach in form vue

--- src/main/vue/contact-form/src/main.ts
+++ src/main/vue/contact-form/src/main.ts
@@ -1,24 +1,20 @@
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
-import ContactForm from './App.vue';
+import App from './App.vue';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function initialize(i18nData: any) {
+(async () => {
+  const app = createApp(App, {
+    objectId: 'dbbs_mods_00000006',
+    baseUrl: 'http://localhost:8291/mir/',
+  });
+  const i18nResponse = await fetch('http://localhost:8291/mir/rsc/locale/translate/digibib.contact.frontend.*');
+  const i18nData = await i18nResponse.json();
   const i18n = createI18n({
     locale: '_',
     messages: {
       _: i18nData,
     },
   });
-  const app = createApp(ContactForm, {
-    objectId: 'dbbs_mods_00000006',
-    baseUrl: 'http://localhost:8291/mir/',
-  });
   app.use(i18n);
   app.mount('#app');
-}
-
-fetch('http://localhost:8291/mir/rsc/locale/translate/digibib.contact.frontend.*')
-  .then((response) => response.json())
-  .then((data) => initialize(data))
-  .catch(() => initialize({}));
+})();
