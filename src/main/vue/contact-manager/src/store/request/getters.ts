366a356f0141d74dd85ce850f33fb19e853ef067
Lennard Golsch <lennard.golsch@icloud.com>
2022-10-07 15:27:56 +0200

    fixes

--- src/main/vue/contact-manager/src/store/request/getters.ts
+++ src/main/vue/contact-manager/src/store/request/getters.ts
@@ -4,11 +4,9 @@ import { Request } from '../../utils';
 import { State } from './state';
 
 export type Getters = {
-  getRequestByUUID(state: State): (slug: string) => Request| undefined;
   getRequests(state: State): Request[];
 }
 
 export const getters: GetterTree<State, RootState> & Getters = {
-  getRequestByUUID: (state: State) => (slug: string) => state.requests.find((r) => r.uuid === slug),
   getRequests: (state: State) => Object.values(state.cache),
 };
