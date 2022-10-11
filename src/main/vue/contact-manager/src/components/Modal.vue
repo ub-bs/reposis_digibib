b035b0aed060aba75e22924cb9231a7a6d78947f
Lennard Golsch <lennard.golsch@icloud.com>
2022-10-11 13:29:42 +0200

    fixed modal centered

--- src/main/vue/contact-manager/src/components/Modal.vue
+++ src/main/vue/contact-manager/src/components/Modal.vue
@@ -1,35 +1,33 @@
 <template>
-  <Transition name="modal-fade">
-    <div class="modal-backdrop" @click="close">
-      <div class="modal-dialog" :class="style" role="document" @click.stop="">
-        <div class="modal-content">
-          <div class="modal-header">
-            <slot name="title">
-              <h5 class="modal-title">
-                {{ title }}
-              </h5>
-              <button v-if="!hideHeaderClose" type="button" class="close" aria-label="Close">
-                <span aria-hidden="true" @click="close">&times;</span>
-              </button>
-            </slot>
-          </div>
-          <div class="modal-body">
-            <slot />
-          </div>
-          <div class="modal-footer">
-            <slot name="footer">
-              <button v-if="!okOnly" type="button" class="btn btn-secondary" @click="cancel">
-                {{ cancelTitle }}
-              </button>
-              <button type="button" class="btn btn-primary" @click="ok">
-                {{ okTitle }}
-              </button>
-            </slot>
-          </div>
+  <div class="modal-backdrop" @click="close">
+    <div class="modal-dialog modal-dialog-centered" :class="style" role="document" @click.stop="">
+      <div class="modal-content">
+        <div class="modal-header">
+          <slot name="title">
+            <h5 class="modal-title">
+              {{ title }}
+            </h5>
+            <button v-if="!hideHeaderClose" type="button" class="close" aria-label="Close">
+              <span aria-hidden="true" @click="close">&times;</span>
+            </button>
+          </slot>
+        </div>
+        <div class="modal-body">
+          <slot />
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+            <button v-if="!okOnly" type="button" class="btn btn-secondary" @click="cancel">
+              {{ cancelTitle }}
+            </button>
+            <button type="button" class="btn btn-primary" @click="ok">
+              {{ okTitle }}
+            </button>
+          </slot>
         </div>
       </div>
     </div>
-  </Transition>
+  </div>
 </template>
 <script setup lang="ts">
 import { computed } from 'vue';
@@ -91,24 +89,6 @@ const style = computed(() => {
 </script>
 <style>
 .modal-backdrop {
-  z-index: 9998;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
   background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.modal-fade-enter-from {
-  opacity: 0;
-}
-.modal-fade-enter-active,
-.modal-fade-leave-active,
-.modal-fade-leave-from,
-.modal-fade-leave-to {
-  transition: all 0.3s ease-out;
 }
 </style>
