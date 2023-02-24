ae15d81a7fc5f8da3fd9526b9ac44ff335da40ab
Lennard Golsch <36492865+golsch@users.noreply.github.com>
2023-02-24 09:33:51 +0100

    Added CodeMeta and advanced metadata fields for research data (#7)
    * Added CodeMeta and advanced metadata fields for research data
    * Removed genre filter in admin mask
    * Added CodeMeta set
    * Fixed typos
    * fixed name because of mir-1144
    * fixed versionnumber to version
    * switched to isreferencedby
    * fixed marcgt
    * fixed class
    * fixed internal related publications
    * improved related items for codemeta
    * fixes
    * removed ruleset
    * fixed label
    * fixed genre bug
    * added static transformer for codemeta

---
+++ src/main/java/de/gbv/reposis/util/ValidationHelper.java
@@ -0,0 +1,17 @@
+package de.gbv.reposis.util;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+public class ValidationHelper {
+
+    public static boolean validateURL(final String url) {
+        try {
+            new URL(url).toURI();
+        } catch (MalformedURLException | URISyntaxException e) {
+            return false;
+        }
+        return true;
+    }
+}
