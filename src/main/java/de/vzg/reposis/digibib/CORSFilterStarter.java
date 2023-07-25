03a79a306fa9dd5890aa79e5416dc769034db149
Lennard Golsch <lennard.golsch@icloud.com>
2023-07-25 13:51:56 +0200

    fixed jakarta

--- src/main/java/de/vzg/reposis/digibib/CORSFilterStarter.java
+++ src/main/java/de/vzg/reposis/digibib/CORSFilterStarter.java
@@ -2,9 +2,9 @@ package de.vzg.reposis.digibib;
 
 import org.mycore.common.events.MCRStartupHandler;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
