a3444207e250f45ce9d23f980181e7963f737143
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2024-03-14 18:10:24 +0100

    Remove old javascript code (replace with currentUser placeholder)

--- src/main/resources/META-INF/resources/js/digibib.js
+++ src/main/resources/META-INF/resources/js/digibib.js
@@ -32,10 +32,6 @@ $(document).ready(function() {
     };
   }
 
-  // replace placeholder USERNAME with username
-  var userID = $("#currentUser strong").html();
-  var newHref = 'https://leopard.tu-braunschweig.de/servlets/solr/select?q=state%3Asubmitted%20AND%20createdby:' + userID + '&fq=objectType:mods';
-  $("a[href='https://leopard.tu-braunschweig.de/servlets/solr/select?q=state%3Asubmitted%20AND%20createdby:USERNAME']").attr('href', newHref);
 // spam protection for mails
   $('span.madress').each(function(i) {
       var text = $(this).text();
